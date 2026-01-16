a36298375744f542527598b1e4f0b02509123d8e
BNMAMA <ksuyeon56@naver.com>
2026-01-16 14:27:31 +0900

    <docs>(global): sidebar modified

--- Tutoroo_project_front/src/pages/mypage/SidebarStyle.js
+++ Tutoroo_project_front/src/pages/mypage/SidebarStyle.js
@@ -4,7 +4,7 @@ export const sidebarContainer = css`
     
     width: 250px;
     margin-right: 250px;
-    flex-shrink: 0; //화면 줄어도 사이드바 유지
+    flex-shrink: 0; 
    
 `;
 
@@ -28,7 +28,7 @@ export const menuItem = css`
     border-bottom: 1px solid #eee;
 `;
 
-// 기본 링크 스타일
+
 export const menuLink = css`
     display: block;
     text-decoration: none;
@@ -36,7 +36,7 @@ export const menuLink = css`
     padding: 18px 20px;
     font-size: 24px;
     transition: all 0.2s;
-    border-left: 4px solid transparent; /* 활성화 시 border 생성을 위해 미리 공간 확보 */
+    border-left: 4px solid transparent; 
 
     &:hover {
         background-color: #f9f9f9;
@@ -44,10 +44,10 @@ export const menuLink = css`
     }
 `;
 
-// 활성화(클릭됨) 되었을 때 추가되는 스타일
+
 export const activeLink = css`
-    background-color: #FFF8F0; /* 연한 주황색 배경 */
+    background-color: #FFF8F0; 
     color: #333;
     font-weight: 600;
-    border-left: 4px solid #FF8A3D; /* 진한 주황색 포인트 바 */
+    border-left: 4px solid #FF8A3D; 
 `;
